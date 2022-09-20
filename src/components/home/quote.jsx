e7118d23ee5ce3b156b51ebfd7f8afba95b866eb
James Lowther <jamesevlowther@gmail.com>
2022-09-19 23:35:37 -0600

    Update color

--- src/components/home/quote.jsx
+++ src/components/home/quote.jsx
@@ -4,7 +4,7 @@ const Quote = () => {
   return (
     <>
       <p
-        className="text-xl text-center mt-8 mb-6 text-stone-900 dark:text-stone-100 dark:bg-zinc-800 bg-neutral-100 mx-auto w-full sm:w-1/2 md:w-1/3 xl:w-1/4 pt-1 pb-2 px-2 rounded-lg shadow-lg outline outline-2 outline-neutral-300"
+        className="text-xl text-center mt-8 mb-6 text-stone-900 dark:text-stone-100 dark:bg-zinc-800 bg-neutral-50 mx-auto w-full sm:w-1/2 md:w-1/3 xl:w-1/4 pt-1 pb-2 px-2 rounded-lg shadow-lg outline outline-2 outline-neutral-300"
         data-sal="fade"
         data-sal-duration="1000"
         data-sal-delay="900"
